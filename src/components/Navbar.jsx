a6c3bc686e823da5ac92e9485fb5b867c52a5dee
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-24 20:56:11 +0700

    navbar preview

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
-import { IoIosMenu } from "react-icons/io";
-
+import { useSelector, useDispatch } from "react-redux";
+import { showNav, hideNav } from "../actions";
+import { IoIosMenu, IoIosClose } from "react-icons/io";
 import logo from "../assets/images/zemestagame-logo.png";
 
 const Navbar = () => {
+  const nav = useSelector((state) => state.nav);
+  const dispatch = useDispatch();
+
+  const showNavbar = () => {
+    dispatch(showNav());
+  };
+
+  const hideNavbar = () => {
+    dispatch(hideNav());
+  };
+
   return (
     <div className="navbar">
       <div className="container">
@@ -10,9 +22,20 @@ const Navbar = () => {
           <img src={logo} alt="" />
         </a>
 
-        <IoIosMenu className="mobile-menu" />
+        <IoIosMenu className="mobile-menu" onClick={showNavbar} />
 
-        <nav></nav>
+        <nav className={nav ? "active" : "menu-btn"}>
+          <IoIosClose className="mobile-menu-exit" onClick={hideNavbar} />
+
+          <ul>
+            <li>
+              <a href="/">HOME</a>
+            </li>
+            <li>
+              <a href="/">CONTEST</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
