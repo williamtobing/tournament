7dab9b3ad055c0046dc848fed0ec98dca0470aad
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-24 17:16:08 +0700

    reg preview

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
+import shadow from "../assets/images/grad.png";
+
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="container"></div>
+      <div className="container">
+        <div className="hero-wrapper">
+          <div className="date-wrapper">
+            <div className="text">
+              <p>REGISTRATION</p>
+              <p className="tba">TBA</p>
+            </div>
+          </div>
+          <div className="hero-graphic"></div>
+        </div>
+        <img src={shadow} alt="" />
+      </div>
     </div>
   );
 };
