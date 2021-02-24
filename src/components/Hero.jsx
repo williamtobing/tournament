aea30b3335982610912eb699ac84c70e6da755fc
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-24 19:44:00 +0700

    hero preview

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,24 +5,13 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="container">
-        <div className="hero-wrapper">
-          <div className="date-wrapper">
-            <div className="text">
-              <p>REGISTRATION</p>
-              <p className="tba">TBA</p>
-            </div>
-          </div>
-          <div className="hero-graphic">
-            <div className="content">
-              <h2>ZEMESTAGAME</h2>
-              <h1>PUBGM</h1>
-              <img src={character} alt="" />
-              <h2 className="end">CHAMPIONSHIP</h2>
-            </div>
-          </div>
-          <div className="circle"></div>
+        {/* <p>REGISTRATION</p>
+          <p>TBA</p> */}
+        <div className="event">
+          <h2>ZEMESTAGAME</h2>
+          <h1>PUBGM</h1>
+          <h2>CHAMPIONSHIP</h2>
         </div>
-        <img src={shadow} alt="" />
       </div>
     </div>
   );
