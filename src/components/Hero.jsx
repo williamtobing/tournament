c2e81e52ed11f00b5b83b954fcb6f6e53a7c1583
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-24 22:30:41 +0700

    character preview

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,20 @@
 import shadow from "../assets/images/grad.png";
-// import character from "../assets/images/pubg-character.png";
+import character from "../assets/images/pubg-character.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="container">
         {/* <p>REGISTRATION</p>
-          <p>TBA</p> */}
+        <p>TBA</p> */}
         <div className="event">
           <h2>ZEMESTAGAME</h2>
           <h1>PUBGM</h1>
           <h2>CHAMPIONSHIP</h2>
         </div>
       </div>
-      <img src={shadow} alt="" />
+      <img className="character" src={character} alt="" />
+      <img className="shadow" src={shadow} alt="" />
     </div>
   );
 };
