ba9c2052532dba42116fc81c1e21a8fbd09c4bc8
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-25 15:33:42 +0700

    preview

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import bottomCircle from "../assets/images/bottom-circle.svg";
 import shadow from "../assets/images/grad.webp";
 import character from "../assets/images/pubg-character.webp";
 import Fade from "react-reveal/Fade";
@@ -6,20 +7,26 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="container">
-        {/* <p>REGISTRATION</p>
-        <p>TBA</p> */}
+        <img className="shadow-up" src={shadow} alt="" />
+
         <div className="event">
           <Fade bottom>
-            <h2>ZEMESTAGAME</h2>
+            <h2 className="text-left">ZEMESTAGAME</h2>
             <h1>PUBGM</h1>
-            <h2>CHAMPIONSHIP</h2>
+            <h2 className="text-right">CHAMPIONSHIP</h2>
           </Fade>
         </div>
+
+        <img className="bottom-circle" src={bottomCircle} alt="" />
+        <div className="date">
+          <p>REGISTRATION</p>
+          <p className="time">TBA</p>
+        </div>
+        <Fade right>
+          <img className="character" src={character} alt="" />
+        </Fade>
+        <img className="shadow-down" src={shadow} alt="" />
       </div>
-      <Fade right>
-        <img className="character" src={character} alt="" />
-      </Fade>
-      <img className="shadow" src={shadow} alt="" />
     </div>
   );
 };
