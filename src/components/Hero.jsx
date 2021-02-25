aa63fb839ad06f203499f0b1e7b0ccc4ccaf15ec
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-25 16:17:24 +0700

    hero finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import bottomCircle from "../assets/images/bottom-circle.svg";
 import shadow from "../assets/images/grad.webp";
 import character from "../assets/images/pubg-character.webp";
+import circle from "../assets/images/circle.svg";
 import Fade from "react-reveal/Fade";
 
 const Hero = () => {
@@ -8,6 +9,7 @@ const Hero = () => {
     <div className="hero">
       <div className="container">
         <img className="shadow-up" src={shadow} alt="" />
+        <img className="circle" src={circle} alt="" />
 
         <div className="event">
           <Fade bottom>
@@ -16,17 +18,17 @@ const Hero = () => {
             <h2 className="text-right">CHAMPIONSHIP</h2>
           </Fade>
         </div>
-
-        <img className="bottom-circle" src={bottomCircle} alt="" />
-        <div className="date">
-          <p>REGISTRATION</p>
-          <p className="time">TBA</p>
-        </div>
-        <Fade right>
-          <img className="character" src={character} alt="" />
-        </Fade>
-        <img className="shadow-down" src={shadow} alt="" />
       </div>
+
+      <img className="bottom-circle" src={bottomCircle} alt="" />
+      <div className="date">
+        <p>REGISTRATION</p>
+        <p className="time">TBA</p>
+      </div>
+      <Fade right>
+        <img className="character" src={character} alt="" />
+      </Fade>
+      <img className="shadow-down" src={shadow} alt="" />
     </div>
   );
 };
