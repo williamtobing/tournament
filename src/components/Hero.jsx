43c221a53ca3464818baad9659c06b9c072e96be
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-25 13:37:25 +0700

    animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import shadow from "../assets/images/grad.webp";
 import character from "../assets/images/pubg-character.webp";
+import Fade from "react-reveal/Fade";
 
 const Hero = () => {
   return (
@@ -8,12 +9,16 @@ const Hero = () => {
         {/* <p>REGISTRATION</p>
         <p>TBA</p> */}
         <div className="event">
-          <h2>ZEMESTAGAME</h2>
-          <h1>PUBGM</h1>
-          <h2>CHAMPIONSHIP</h2>
+          <Fade bottom>
+            <h2>ZEMESTAGAME</h2>
+            <h1>PUBGM</h1>
+            <h2>CHAMPIONSHIP</h2>
+          </Fade>
         </div>
       </div>
-      <img className="character" src={character} alt="" />
+      <Fade right>
+        <img className="character" src={character} alt="" />
+      </Fade>
       <img className="shadow" src={shadow} alt="" />
     </div>
   );
